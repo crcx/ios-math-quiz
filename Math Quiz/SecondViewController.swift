b14441ce23494aaac4f1063ae4c38ee9f761a111
Charles Childers <crc@forthworks.com>
2014-10-19 12:06:04 -0400

    Initial Commit

---
+++ Math Quiz/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Math Quiz
+//
+//  Created by Charles Childers on 10/19/14.
+//  Copyright (c) 2014 Charles Childers. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
