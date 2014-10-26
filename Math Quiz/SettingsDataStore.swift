9593afe6465ee8cffbd18fcc844eb147aaa085e9
Charles Childers <crc@forthworks.com>
2014-10-26 10:33:17 -0400

    start a singleton to hold the game settings / state

---
+++ Math Quiz/SettingsDataStore.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsDataStore.swift
+//  Math Quiz
+//
+//  Created by Charles Childers on 10/26/14.
+//  Copyright (c) 2014 Charles Childers. All rights reserved.
+//
+
+import Foundation
+
+class SettingsDataStore {
+    var lower: Int
+    var upper: Int
+    var gameMode: Int
+    
+    struct Static {
+        static var instance: SettingsDataStore?
+    }
+    
+    class var sharedInstance: SettingsDataStore {
+        if !(Static.instance != nil) {
+            Static.instance = SettingsDataStore()
+        }
+        return Static.instance!
+    }
+    
+    init() {
+        self.lower = 0
+        self.upper = 0
+        self.gameMode = 0
+    }
+}
